e12855cc3d6772f062fa6ffee8848c7ff08c7f65
Leonardo R <lfavio2@gmail.com>
2019-09-22 10:21:37 -0500

    se actualiza el módulo users

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import account from './api/users';
+import users from './api/users';
 //Initializations
 const app = express();
 //settings
 const port = process.env.PORT || 3000;
 //Routes
-app.use('/users',account);
+app.use('/users',users);
 //starting the server
 app.listen(port,()=>{
     console.log(`Api rest corriendo en el puerto ${port}`);
