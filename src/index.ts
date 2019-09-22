fd8b6c3ba7589421ffec0f4cfa3f1005b4e9f1aa
Leonardo R <lfavio2@gmail.com>
2019-09-22 00:32:08 -0500

    actualizacion

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import account from './routes/account';
+import account from './routes/users';
 const app = express();
 const port = process.env.PORT || 3000;
 //Routes
--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 const router: Router = Router();
 router.get('/',(req,res)=>res.send('Hola mundo'))
-router.get('/sign_in',(req,res)=>res.send('registrado'))
+router.get('/sign_in',(req,res)=>res.send('connected'))
+router.get('/sign_out',(req,res)=>res.send('disconect'))
 export default router;
