d619ee4790ec9fe75869767c26937f1d3c94bb73
Leonardo R <lfavio2@gmail.com>
2019-09-23 03:07:05 -0500

    actualizacion

---
+++ src/todolist.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE Users(
+   UserId serial PRIMARY KEY,
+   UserName VARCHAR (50) UNIQUE NOT NULL,
+   Password VARCHAR (50) NOT NULL,
+   creationDate TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Activities(
+   ActivityId serial PRIMARY KEY,
+   Description VARCHAR (50) NOT NULL,
+   CreationDate TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Tasks(
+   TaskId serial PRIMARY KEY,
+   UserId integer NOT NULL,
+   ActivityId integer NOT NULL,
+   Message VARCHAR (50) NOT NULL,
+   Status VARCHAR (10) NOT NULL,
+   CreationDate TIMESTAMP NOT NULL
+);
+
+ALTER TABLE Tasks 
+ADD CONSTRAINT user_id FOREIGN KEY (UserId) REFERENCES Users (UserId);
+
+ALTER TABLE Tasks 
+ADD CONSTRAINT activity_id FOREIGN KEY (ActivityId) REFERENCES Activities (ActivityId);
